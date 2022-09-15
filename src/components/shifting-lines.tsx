4a7bef799cbf4eb955dff5e300498e322ec98c70
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-09-16 00:12:22 +0200

    feat: line colors according to sibling cells

--- src/components/shifting-lines.tsx
+++ src/components/shifting-lines.tsx
@@ -2,6 +2,7 @@ import { CSSProperties, FC } from "react";
 
 interface IShiftingLines {
   grid: number[][];
+  colorSetup: TColorSetup;
 }
 
 const cellSize = 20;
@@ -13,6 +14,12 @@ const style: CSSProperties = {
   height: cellSize,
 };
 
+export type TColorSetup = {
+  baseChoices: string[];
+  backgroundLines: string;
+  background: string;
+};
+
 const siblingExists = (grid: number[][], x: number, y: number): boolean => {
   if (grid.length > y) {
     if (grid[0].length > x) {
@@ -22,21 +29,54 @@ const siblingExists = (grid: number[][], x: number, y: number): boolean => {
   return false;
 };
 
+const colorCache: Record<string, string> = {};
+const getCellColor = (
+  x: number,
+  y: number,
+  val: number,
+  colorSetup: TColorSetup
+): string => {
+  if (val == 0) return "";
+  if (val == 2) return colorSetup.backgroundLines;
+
+  const leftSiblingIndex = `${x - 1}x${y}`;
+  const rightSiblingIndex = `${x + 1}x${y}`;
+
+  const colorChoice =
+    leftSiblingIndex in colorCache
+      ? colorCache[leftSiblingIndex]
+      : rightSiblingIndex in colorCache
+      ? colorCache[rightSiblingIndex]
+      : colorSetup.baseChoices[
+          Math.floor(Math.random() * colorSetup.baseChoices.length)
+        ];
+  colorCache[`${x}x${y}`] = colorChoice;
+  return colorChoice;
+};
+
 const globalOffset = [40, 40];
 
-const ShiftingLines: FC<IShiftingLines> = ({ grid }) => {
+const ShiftingLines: FC<IShiftingLines> = ({ grid, colorSetup }) => {
   return (
-    <div className="app">
+    <div
+      className="app"
+      style={{
+        backgroundColor: colorSetup.background,
+        width: grid[0].length * cellSize + globalOffset[0],
+        height: grid.length * 2 * cellSize + globalOffset[1],
+      }}
+    >
       {grid.map((row, y) => {
         return (
           <>
             {row.map((val, x) => {
               const hasRightSibling = siblingExists(grid, x + 1, y);
               const hasLeftSibling = siblingExists(grid, x - 1, y);
-              const color = val ? val == 1 ? 'red' : 'green' : ''
+              const color = getCellColor(x, y, val, colorSetup);
+
               return (
                 <div
-                  key={`cell-${x}-${y}`}
+                  key={`cell-${x}x${y}`}
                   style={{
                     ...style,
                     top: y * cellSize + cellSize * y + globalOffset[1],
